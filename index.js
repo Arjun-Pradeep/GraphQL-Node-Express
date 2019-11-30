a3e8dbadee456f537603de7dc460f0b2a1016de5
Arjun-Pradeep <arjunpradeepm17@gmail.com>
2019-11-30 18:50:14 +0530

    GraphQL_Schema

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import express from 'express';
 
+// A L L O W S    U S    T O    U S E    G R A P H Q L    W I T H    E X P R E S S J S 
+import graphqlHTTP from 'express-graphql'
+
+// I M P O R T    S C H E M A    F I L E 
+import schema from './schema'
+
 const app = express();
 
 app.get('/',(req,res) => {
@@ -7,6 +13,16 @@ app.get('/',(req,res) => {
         msg : "Welcome to GraphQL World !!"
     })
 })
+
+const root = {
+    hello : () => "Welcome to GraphQL"
+}
+
+app.use('/graphql',graphqlHTTP({
+    schema : schema,
+    rootValue : root,
+    graphiql : true
+}))
 
 app.listen(3000,() => {
     console.log("App is running on port 3000");
